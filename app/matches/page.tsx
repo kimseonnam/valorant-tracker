680c45e1f9a1b36a305cb7812cee9632e997199a
kimseonnam <rimmentopro@gmail.com>
2026-04-20 02:10:53 +0900

    fix

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -580,7 +580,8 @@ const handleConfirmMatchSave = () => {
               관리자 로그인
             </Button>
           )}
-        
+        </div>
+      </div>
 
       <AdminLoginDialog 
         open={isLoginDialogOpen} 
